d5e49d9a30bc84d7d8771ff5072972e39a77c0ad
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-12-28 20:14:17 +0530

    Create App.js

---
+++ react-state/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { Grocery } from './Gcomponents/Grocery';
+
+function App() {
+
+  return (
+    <div className="App">
+
+ <Grocery />
+
+    </div>
+  );
+}
+
+// function Child1({handledata}){
+//   const data = "Immortalls";
+//   return handledata(data)
+
+
+// }
+
+// function Child2({data}){
+// return <div>{ data}</div>
+
+// }
+
+
+
+export default App;
