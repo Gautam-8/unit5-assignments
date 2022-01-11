66d5058445d6947e9a64bc2eff1aeeaae39f0e92
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-11 20:21:18 +0530

    Update About.jsx

--- react-routers/src/components/About.jsx
+++ react-routers/src/components/About.jsx
@@ -1,5 +1,9 @@
 export const About = () => {
     return (
-        <h1>About page</h1>
+        <>
+        {/* <h1>About page</h1> */}
+        <h1>Dashboard page</h1>
+        </>
+        
     )
 }
