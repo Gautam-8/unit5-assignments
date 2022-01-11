778a13c18445863f16891205745f6e5bdf9ec7a6
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-11 20:21:39 +0530

    Update Navbar.jsx

--- react-routers/src/components/Navbar.jsx
+++ react-routers/src/components/Navbar.jsx
@@ -7,15 +7,17 @@ export const Navbar = () => {
     
      <Link to="/" style={{margin:"10px" , color:"coral"}}>Home</Link> 
 
-     <Link to="/about" style={{margin:"10px"  , color:"coral"}}>About</Link>
+     <Link to="/about" style={{margin:"10px"  , color:"coral"}}>Dashboard</Link>
 
-     <Link to="/contactus" style={{margin:"10px"  , color:"coral"}}>Contact</Link>
+     <Link to="/contactus" style={{margin:"10px"  , color:"coral"}}>Settings</Link>
 
      {/* <Link to="/products/:id" style={{margin:"10px"  , color:"coral"}}>Product</Link> */}
 
-     <Link to="Users" style={{margin:"10px"  , color:"coral"}}>Users</Link>
+     <Link to="/Users" style={{margin:"10px"  , color:"coral"}}>Users</Link>
 
-     <Link to="products"  style={{margin:"10px"  , color:"coral"}}>Products</Link>
+     <Link to="/products"  style={{margin:"10px"  , color:"coral"}}>Products</Link>
+
+     <Link to="/login"  style={{margin:"10px"  , color:"coral"}}>Login</Link>
 
       </div>
   )
