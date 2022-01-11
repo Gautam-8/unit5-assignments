3bea714fa33e0410bb827ce684eae1d8fbb33674
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-11 20:21:21 +0530

    Update Contact.jsx

--- react-routers/src/components/Contact.jsx
+++ react-routers/src/components/Contact.jsx
@@ -1,5 +1,9 @@
 export const Contact = () => {
     return (
-        <h1>Contact Us</h1>
+        <>
+        {/* <h1>Contact Us</h1> */}
+        <h1>Settings page</h1>
+        </>
+        
     )
 }
