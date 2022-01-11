a914930d65ba97764102758f914d5dabc7b3d08e
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-11 20:21:50 +0530

    Update UserDetails.jsx

--- react-routers/src/components/UserDetails.jsx
+++ react-routers/src/components/UserDetails.jsx
@@ -1,11 +1,14 @@
-import { useParams } from "react-router-dom"
-import { useEffect, useState } from "react";
+import { useParams ,useNavigate, Navigate } from "react-router-dom"
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/Auth.Context";
 
 export const UserDetails = () => {
 
     const { userid } = useParams();
     
-    const [user , setUser ] = useState({})
+    const [user , setUser ] = useState({});
+
+    const {token} = useContext(AuthContext)
 
     useEffect(() => {
       fetch(`https://reqres.in/api/users/${userid}`)
@@ -13,13 +16,19 @@ export const UserDetails = () => {
         setUser(d.data)
         console.log(d.data)
       })
-    } ,[userid]) 
+    } ,[userid]) ;
+
+    // if(!token){
+    //   return <Navigate to={'/login'} />
+    // }
 
    return(
        <div>
      
-       <img src={user.avatar} />
+       <img src={user.avatar} alt="test"/>
         <h5>{user.first_name}</h5>
+        <h5>{user.last_name}</h5>
+        Contact:{user.email}
 
        </div>
    )
